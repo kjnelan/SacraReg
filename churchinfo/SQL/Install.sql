691e2ad631b95aaeacf4148d7a36d1a69d3ad4e0
Michael Wilt <mike@mwilt.org>
2016-07-24 18:35:49 -0400

    Created a little page that is designed to be embedded in a larger page to show fundraising progress

--- churchinfo/SQL/Install.sql
+++ churchinfo/SQL/Install.sql
@@ -173,6 +173,7 @@ INSERT INTO `config_cfg` (`cfg_id`, `cfg_name`, `cfg_value`, `cfg_type`, `cfg_de
 (73, 'sElectronicTransactionProcessor', 'Vanco', 'text', 'Vanco', 'Electronic Transaction Processor', 'General', NULL),
 (74, 'sSelfServiceFunds', '"Pledge Receipts"', 'text', '"Pledge Receipts"', 'Donation funds to present through self-service', 'General', NULL),
 (75, 'sSelfServiceAdminEmails', '', 'text', '', 'Comma-separated list of emails to distribute self-service notifications', 'General', NULL),
+(76, 'sBroadcastFunds', '', 'text', '', 'Comma-separated list of funds to broadcast progress through the embedded web page', 'General', NULL),
 (999, 'bRegistered', '0', 'boolean', '0', 'ChurchInfo has been registered.  The ChurchInfo team uses registration information to track usage.  This information is kept confidential and never released or sold.  If this field is true the registration option in the admin menu changes to update registration.', 'General', NULL),
 (1001, 'leftX', '20', 'number', '20', 'Left Margin (1 = 1/100th inch)', 'ChurchInfoReport', NULL),
 (1002, 'incrementY', '4', 'number', '4', 'Line Thickness (1 = 1/100th inch', 'ChurchInfoReport', NULL),
