7a477c47906fa3e0af811466f0d9963e71f980a6
Michael Wilt <mike@mwilt.org>
2018-11-13 20:54:58 -0500

    Replaced call to an obsolete function with a supported replacement

--- churchinfo/Reports/AdvancedDeposit.php
+++ churchinfo/Reports/AdvancedDeposit.php
@@ -838,7 +838,7 @@ if ($output == "pdf") {
 	$eol = "\r\n";
 	
 	// Build headings row
-	eregi ("SELECT (.*) FROM ", $sSQL, $result);
+	preg_match  ("/SELECT DISTINCT (.*) FROM /", $sSQL, $result);
 	$headings = explode(",",$result[1]);
 	$buffer = "";
 	foreach ($headings as $heading) {
